39ee0a2b36993105f3cf7e792c66ebf3cf5be610
Julian Mason <julian@Julians-MacBook-Air-2.local>
2022-12-22 14:47:33 -0800

    challenge03

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn)
+
+var passwordLength = 8-128;
+var lowerCase ="abcdefghijklmnopqrstuvwxyz";
+var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacters ="!@#$%^&*()+=<>";
+var numbers = "0123456789";
+let allChar = "";
 
 // Write password to the #password input
+function generatePassword() {
+  console.log("btn was clicked")
+  let passwrdLength = prompt(
+    "choose a number between 8 and 128"
+  );
+  if (passwordLength < 8 || passwordLength > 128 ) {
+    alert("enter a number between 8 and 128.");
+  } else {
+    let lowerCase = confirm("Would you like to use lowerCase letters?");
+    if(lowerCase){
+      allChar += lowerCase
+    };
+    let upperCase = confirm("Would you like to use upperCase letters?");
+    if (upperCase){
+      allChar += upperCase
+    };
+    let specialCharacters = confirm("Would you like to use specialCharacters?");
+    if (specialCharacters){
+      allChar += specialCharacters
+    };
+    let numbers = confirm("Would you like to use numbers?");
+    if (numbers){
+      allChar += numbers
+    };
+   
+   return result }
+  }
+
+  let password = "";
+  for (var i = 0; i < passwordLength; i++){
+    password += allChar.charAt(Math.floor(Math.random() * allChar.length))
+  }
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-   
-  
+
   passwordText.value = password;
-  
+
 }
-function generatePassword() {
-  var paawordsLength = 8;
-  var characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY";
-  var password = Math.floor(Math.random() * characters.length);
-}
-for (var i = 0; i < passwordlength; i++) {
-  password += characters(Math.floor(Math.random() * characters.length));
-}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 console.log(generateBtn)
