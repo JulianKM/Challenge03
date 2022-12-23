273469e0368d189c3e2a5475c805ed965916c19f
Julian Mason <julian@Julians-MacBook-Air-2.local>
2022-12-22 21:56:38 -0800

    challenge03

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ console.log(generateBtn)
 var passwordLength = 8-128;
 var lowerCase ="abcdefghijklmnopqrstuvwxyz";
 var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789"
 var specialCharacters ="!@#$%^&*()+=<>";
-var numbers = "0123456789";
 let all = "";
 
 // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
 function generatePassword() {
   let passwrdLength = prompt(
     "choose a number between 8 and 128"
@@ -17,19 +25,19 @@ function generatePassword() {
   if (passwordLength < 8 || passwordLength > 128 ) {
     prompt("enter a number between 8 and 128.");
   } else {
-    let lowerCase = confirm("Would you like to use lowerCase letters?");
+    let lowerCase = confirm("use lowerCase letters?");
     if(lowerCase){
       all += lowerCase
     };
-    let upperCase = confirm("Would you like to use upperCase letters?");
+    let upperCase = confirm("use upperCase letters?");
     if (upperCase){
       all += upperCase
     };
-    let specialCharacters = confirm("Would you like to use specialCharacters?");
+    let specialCharacters = confirm("use specialCharacters?");
     if (specialCharacters){
       all += specialCharacters
     };
-    let numbers = confirm("Would you like to use numbers?");
+    let numbers = confirm("use numbers?");
     if (numbers){
       all += numbers
     };
@@ -42,14 +50,5 @@ function generatePassword() {
     password += all.charAt(Math.floor(Math.random() * all.length))
   }
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
+  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-console.log(generateBtn)
